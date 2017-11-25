574517eda473c7a5e332d06a58b74c94442db203
Brian MacDonald <brianjmacdonald@gmail.com>
2017-11-25 15:36:00 -0500

    Setting Up GUI WinForm elements

--- MaintenanceAllocationChartDeveloper/Form1.Designer.cs
+++ MaintenanceAllocationChartDeveloper/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MaintenanceAllocationChartDeveloper
 {
-    partial class Form1
+    partial class frmMacDevMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMacDevMain));
+            this.GroupNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CompAssy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaintFunct = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.projectSettings = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.fileToolStripDropDownButton = new System.Windows.Forms.ToolStripDropDownButton();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.newFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.howToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportBugToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.lblVersionNum = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.toolStripMenu.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // GroupNo
+            // 
+            this.GroupNo.HeaderText = "GROUP NO.";
+            this.GroupNo.Name = "GroupNo";
+            this.GroupNo.ReadOnly = true;
+            // 
+            // CompAssy
+            // 
+            this.CompAssy.HeaderText = "COMPONENT/ASSEMBLY";
+            this.CompAssy.Name = "CompAssy";
+            this.CompAssy.ReadOnly = true;
+            // 
+            // MaintFunct
+            // 
+            this.MaintFunct.HeaderText = "MAINTENANCE FUNCTION";
+            this.MaintFunct.Name = "MaintFunct";
+            this.MaintFunct.ReadOnly = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.projectSettings);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1199, 550);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // projectSettings
+            // 
+            this.projectSettings.Location = new System.Drawing.Point(4, 22);
+            this.projectSettings.Name = "projectSettings";
+            this.projectSettings.Padding = new System.Windows.Forms.Padding(3);
+            this.projectSettings.Size = new System.Drawing.Size(1191, 524);
+            this.projectSettings.TabIndex = 0;
+            this.projectSettings.Text = "Project Settings";
+            this.projectSettings.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1191, 508);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "MAC Table Editor";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripDropDownButton,
+            this.toolStripDropDownButton1,
+            this.toolStripButton1});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Size = new System.Drawing.Size(1223, 25);
+            this.toolStripMenu.TabIndex = 1;
+            this.toolStripMenu.Text = "Menu";
+            // 
+            // fileToolStripDropDownButton
+            // 
+            this.fileToolStripDropDownButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fileToolStripDropDownButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newFileToolStripMenuItem,
+            this.openFileToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.saveFileToolStripMenuItem,
+            this.saveAsFileToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.quitToolStripMenuItem});
+            this.fileToolStripDropDownButton.Image = ((System.Drawing.Image)(resources.GetObject("fileToolStripDropDownButton.Image")));
+            this.fileToolStripDropDownButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.fileToolStripDropDownButton.Name = "fileToolStripDropDownButton";
+            this.fileToolStripDropDownButton.Size = new System.Drawing.Size(38, 22);
+            this.fileToolStripDropDownButton.Text = "&File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.openFileToolStripMenuItem.Text = "&Open";
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.saveFileToolStripMenuItem.Text = "saveToolStripMenuItem";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(196, 6);
+            // 
+            // newFileToolStripMenuItem
+            // 
+            this.newFileToolStripMenuItem.Name = "newFileToolStripMenuItem";
+            this.newFileToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.newFileToolStripMenuItem.Text = "&New";
+            // 
+            // saveAsFileToolStripMenuItem
+            // 
+            this.saveAsFileToolStripMenuItem.Name = "saveAsFileToolStripMenuItem";
+            this.saveAsFileToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.saveAsFileToolStripMenuItem.Text = "Save &As...";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(196, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.quitToolStripMenuItem.Text = "&Quit Application";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToToolStripMenuItem,
+            this.reportBugToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.toolStripMenuItem1});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(45, 22);
+            this.toolStripDropDownButton1.Text = "&Help";
+            // 
+            // howToToolStripMenuItem
+            // 
+            this.howToToolStripMenuItem.Name = "howToToolStripMenuItem";
+            this.howToToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.howToToolStripMenuItem.Text = "How To &Use This Tool...";
+            // 
+            // reportBugToolStripMenuItem
+            // 
+            this.reportBugToolStripMenuItem.Name = "reportBugToolStripMenuItem";
+            this.reportBugToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.reportBugToolStripMenuItem.Text = "&Report Bug";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(194, 6);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(197, 22);
+            this.toolStripMenuItem1.Text = "About...";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton1.BackColor = System.Drawing.Color.SteelBlue;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(72, 22);
+            this.toolStripButton1.Text = "Save + Quit";
+            // 
+            // lblVersionNum
+            // 
+            this.lblVersionNum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblVersionNum.AutoSize = true;
+            this.lblVersionNum.Location = new System.Drawing.Point(1138, 583);
+            this.lblVersionNum.Name = "lblVersionNum";
+            this.lblVersionNum.Size = new System.Drawing.Size(69, 13);
+            this.lblVersionNum.TabIndex = 2;
+            this.lblVersionNum.Text = "Version: 0.1a";
+            this.lblVersionNum.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // frmMacDevMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1223, 605);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.lblVersionNum);
+            this.Controls.Add(this.toolStripMenu);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "frmMacDevMain";
+            this.Text = "TRG Maintenance Allocation Chart Development Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.DataGridViewTextBoxColumn GroupNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CompAssy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaintFunct;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage projectSettings;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripDropDownButton fileToolStripDropDownButton;
+        private System.Windows.Forms.ToolStripMenuItem newFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem howToToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportBugToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Label lblVersionNum;
     }
 }
 
--- MaintenanceAllocationChartDeveloper/Form1.cs
+++ MaintenanceAllocationChartDeveloper/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace MaintenanceAllocationChartDeveloper
 {
-    public partial class Form1 : Form
+    public partial class frmMacDevMain : Form
     {
-        public Form1()
+        public frmMacDevMain()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
